b92b767f66d6f379f7072352570171ee1bfed468
zxiou <zxiou@MacBook-Pro-3.local>
2016-01-03 19:44:41 +0800

    understand how stage works

--- 2.php
+++ 2.php
@@ -4,9 +4,7 @@ function a() { echo "a\n"; }
 register_tick_function('a');
 declare(ticks = 1);
 
-echo "lcxazx";
-echo "lcx i love you";
-echo "hello git";
+echo "zxalcx";
 echo "zxiou";
 echo "TCZX";
 ?>
